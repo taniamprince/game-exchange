133b6decfd3fd79af46c4cd53012233d605e6d7f
grogers1993 <grogers1993@gmail.com>
2015-10-21 19:15:12 -0400

    Changed OrderProcessingServiceBean.java

--- src/edu/osu/cse5234/business/OrderProcessingServiceBean.java
+++ src/edu/osu/cse5234/business/OrderProcessingServiceBean.java
@@ -3,6 +3,8 @@ package edu.osu.cse5234.business;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 
+import edu.osu.cse5234.model.Order;
+
 /**
  * Session Bean implementation class OrderProcessingServiceBean
  */
@@ -15,6 +17,10 @@ public class OrderProcessingServiceBean {
      */
     public OrderProcessingServiceBean() {
         // TODO Auto-generated constructor stub
+    }
+    
+    public String processOrder(Order order) {
+    	return "";
     }
 
 }
